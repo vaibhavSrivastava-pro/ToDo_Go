ef40a177256c26d1c368f6e2d181beebcb0d0f35
vaibhavSrivastava-pro <vaibhavsrivastava.pro@gmail.com>
2024-10-23 17:56:35 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("Hello")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello World"})
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
